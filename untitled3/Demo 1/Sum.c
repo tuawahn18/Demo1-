9662690b808a000da33b9ffd627d8e618b5c0166
tuawahn18 <Tuaasnanh188@gmail.com>
2023-09-11 20:18:31 +0700

    Demo1

---
+++ untitled3/Demo 1/Sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+int hello_main()
+//int main()
+{
+    printf(" Hello World!");
+    return 0;
+}
---
+++ untitled3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+//int sum_main()
+int main()
+{
+    int a, b, sum;
+    printf(" a=n");
+    scanf("%d", &a);
+    printf(" b=m");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("the sum is: %d", sum);
+    return 0;
+
+}
